9b847714ff6fab577f90c4b271abc0685561a70d
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2024-07-15 22:08:39 -0600

    chore: Add boolean expressions challenge solution

--- CsharpProjects/part3/booleanExpressions/Program.cs
+++ CsharpProjects/part3/booleanExpressions/Program.cs
@@ -6,3 +6,21 @@ Random coinFlip = new Random();
 int flip = coinFlip.Next(1,3);
 
 Console.WriteLine( flip == 2 ? "Heads" : "Tails");
+
+//Permission and lavel values challenge
+
+string permission = "Admin|Manager";
+int level = 55;
+
+
+if (permission.Contains("Admin")){
+    if(level > 55)
+        Console.WriteLine("Welcome, Super Admin user");
+    else 
+        Console.WriteLine("Welcome, Admin user");
+} else if(permission.Contains("Manager")){
+    if (level >= 20)
+        Console.WriteLine("Contact an Admin for access");
+    else 
+        Console.WriteLine("You do not have sufficient privileges");
+}
