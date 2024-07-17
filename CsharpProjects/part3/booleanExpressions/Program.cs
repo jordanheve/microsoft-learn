23d7863ee16c8a19e038c0c54cb5dad9d41c2353
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2024-07-17 07:39:16 -0600

    Refactor code to calculate the sum of numbers in an array and check if it contains the number 42

--- CsharpProjects/part3/booleanExpressions/Program.cs
+++ CsharpProjects/part3/booleanExpressions/Program.cs
@@ -24,3 +24,28 @@ if (permission.Contains("Admin")){
     else 
         Console.WriteLine("You do not have sufficient privileges");
 }
+
+// Code challenge: update problematic code in the code editor
+int[] numbers = [4, 8, 15, 16, 23, 42];
+bool found = false;
+int total = 0;
+foreach (int number in numbers)
+{
+
+    total += number;
+
+    if (number == 42)
+    {
+       found = true;
+
+    }
+
+}
+
+if (found) 
+{
+    Console.WriteLine("Set contains 42");
+
+}
+
+Console.WriteLine($"Total: {total}");
