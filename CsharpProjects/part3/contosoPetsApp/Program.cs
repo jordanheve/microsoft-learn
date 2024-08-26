26df633066611734a9f681144b97d10fe8c164ee
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2024-08-25 20:45:22 -0600

    chore: Refactor code to use switch statements for pet initialization

--- CsharpProjects/part3/contosoPetsApp/Program.cs
+++ CsharpProjects/part3/contosoPetsApp/Program.cs
@@ -20,50 +20,52 @@ string[,] ourAnimals = new string[maxPets, 6];
 for (int i = 0; i < maxPets; i++)
 {   
 
-    if (i == 0)
-    {
-        animalSpecies = "dog";
-        animalID = "d1";
-        animalAge = "2";
-        animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
-        animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
-        animalNickname = "lola";
-    }
-    else if (i == 1)
-    {
-        animalSpecies = "dog";
-        animalID = "d2";
-        animalAge = "9";
-        animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
-        animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
-        animalNickname = "loki";
-    }
-    else if (i == 2)
-    {
-        animalSpecies = "cat";
-        animalID = "c3";
-        animalAge = "1";
-        animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
-        animalPersonalityDescription = "friendly";
-        animalNickname = "Puss";
-    }
-    else if (i == 3)
-    {
-        animalSpecies = "cat";
-        animalID = "c4";
-        animalAge = "?";
-        animalPhysicalDescription = "";
-        animalPersonalityDescription = "";
-        animalNickname = "";
-    }
-    else
-    {
-        animalSpecies = "";
-        animalID = "";
-        animalAge = "";
-        animalPhysicalDescription = "";
-        animalPersonalityDescription = "";
-        animalNickname = "";
+    switch(i){
+        case 0:
+            animalSpecies = "dog";
+            animalID = "d1";
+            animalAge = "2";
+            animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+            animalNickname = "lola";
+        break;
+        case 1:
+            animalSpecies = "dog";
+            animalID = "d2";
+            animalAge = "9";
+            animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+            animalNickname = "loki";
+        break;
+
+        case 2:
+            animalSpecies = "cat";
+            animalID = "c3";
+            animalAge = "1";
+            animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
+            animalPersonalityDescription = "friendly";
+            animalNickname = "Puss";
+        break;
+
+        case 3: 
+            animalSpecies = "cat";
+            animalID = "c4";
+            animalAge = "?";
+            animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "";
+        break;
+
+        default:
+            animalSpecies = "";
+            animalID = "";
+            animalAge = "";
+            animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "";
+        break;
+
+    
     }
 
     ourAnimals[i, 0] = "ID #: " + animalID;
@@ -75,6 +77,8 @@ for (int i = 0; i < maxPets; i++)
 }
 
 // display the top-level menu options
+do 
+{
 
 Console.Clear();
 
@@ -98,6 +102,71 @@ if (readResult != null)
 
 Console.WriteLine($"You selected menu option {menuSelection}.");
 Console.WriteLine("Press the Enter key to continue");
-
 // pause code execution
 readResult = Console.ReadLine();
+
+switch(menuSelection)
+{
+    case "1":
+          // List all of our current pet information
+        for(int i = 0; i < maxPets; i++)
+        {
+            if (ourAnimals[i, 0] != "ID #: ")
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 6; j++)
+                {
+                    Console.WriteLine(ourAnimals[i, j]);
+                }
+            }
+        }
+        Console.WriteLine("Press the Enter key to continue.");
+        readResult = Console.ReadLine();
+        break;
+    case "2":
+        Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+        Console.WriteLine("Press the Enter key to continue.");
+        readResult = Console.ReadLine();
+        break;
+    case "3":
+        Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+        Console.WriteLine("Press the Enter key to continue.");
+        readResult = Console.ReadLine();
+        break;
+    case "4":
+        Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+        Console.WriteLine("Press the Enter key to continue.");
+        readResult = Console.ReadLine();
+        break;
+    case "5":
+        Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+        Console.WriteLine("Press the Enter key to continue.");
+        readResult = Console.ReadLine();
+        break;
+    case "6":
+        Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+        Console.WriteLine("Press the Enter key to continue.");
+        readResult = Console.ReadLine();
+        break;
+    case "7":
+        Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+        Console.WriteLine("Press the Enter key to continue.");
+        readResult = Console.ReadLine();
+        break;
+    case "8":
+        Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+        Console.WriteLine("Press the Enter key to continue.");
+        readResult = Console.ReadLine();
+        break;
+    default: 
+        Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+        Console.WriteLine("Press the Enter key to continue.");
+        readResult = Console.ReadLine();
+        break;
+}
+
+
+
+
+
+}while (menuSelection != "exit");
